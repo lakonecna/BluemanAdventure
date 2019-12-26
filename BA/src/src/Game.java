1fb78ed36b0d6b403074f0c37bb40116337ad3bb
Lucie <lucie.konecna@ucdconnect.ie>
2019-12-26 17:04:44 +0000

    continued work on gdd and experimenting with project structure

---
+++ BA/src/src/Game.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Game extends JFrame {
+    public Game() {
+
+    }
+    public static void main(String[] args) {
+        this = new JFrame("BluemanAdventure");
+
+    }
+}

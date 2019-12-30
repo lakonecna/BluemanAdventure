7b9f9da8f4996e530612e85b400c187e5b7bf0b1
Lucie <lucie.konecna@ucdconnect.ie>
2019-12-30 23:30:41 +0000

    can't remember

--- BAB/StarField/src/main/Game.java
+++ BAB/StarField/src/main/Game.java
@@ -1,4 +1,15 @@
 package main;
 
+import javax.swing.JFrame;
+
 public class Game {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("StarField Program");
+        frame.setContentPane(new GamePanel());
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.pack();
+    }
 }
--- BAB/StarField/src/main/GamePanel.java
+++ BAB/StarField/src/main/GamePanel.java
@@ -1,5 +1,34 @@
 package main;
 
-public class GamePanel {
-    
+import javax.swing.*;
+import java.awt.*;
+import java.util.Random;
+
+public class GamePanel extends JPanel {
+    private static final long serialVersionUID = 1L;
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
+    public GamePanel() {
+        super();
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setFocusable(true);
+        requestFocus();
+    }
+
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.setColor(Color.cyan);
+        g.fillRect(0,0, WIDTH, HEIGHT);
+
+        Random rand = new Random();
+        for(int i = 0; i < 1000; ++i) {
+            Color randCol = new Color(rand.nextInt(256), rand.nextInt(256), rand.nextInt(256));
+            g.setColor(randCol);
+            int x = Math.abs(rand.nextInt() % WIDTH);
+            int y = Math.abs(rand.nextInt() % HEIGHT);
+            System.out.println(x + " " + y);
+            g.drawLine( x, y, x, y);
+        }
+    }
 }

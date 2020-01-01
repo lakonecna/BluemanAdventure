7f3bad783304d678632f59ad4906bc3bb50e57aa
Lucie <lucie.konecna@ucdconnect.ie>
2020-01-01 23:23:32 +0000

    Ant try 1: Unsuccessful

---
+++ BAB/TestAntWithIntelliJ/src/Project.java
@@ -0,0 +1,11 @@
+package main;
+//TODO:
+// encountering problems in this exercise
+// probably related to paths
+// starts on page 79
+public class Project {
+    public static void main(String[] args) {
+        // fitting question
+        System.out.println("Wanna dance? Or code?");
+    }
+}

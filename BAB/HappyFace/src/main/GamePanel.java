f5a2ca044cb8010e97a69236ccec3ead9ac3b764
Lucie <lucie.konecna@ucdconnect.ie>
2020-01-01 12:49:59 +0000

    playing with painting shapes

---
+++ BAB/HappyFace/src/main/GamePanel.java
@@ -0,0 +1,18 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel {
+    public static final long serialVersionUID = 1L;
+    public static final int WIDTH = 640;
+    public static final int HEIGHT = 480;
+
+    public GamePanel() {
+        setPreferredSize(new Dimensions());
+    }
+
+    private void paintComponent(Graphics g) {
+
+    }
+}

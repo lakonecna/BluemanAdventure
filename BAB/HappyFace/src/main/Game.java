1995eae111e1281b98f395208d5266ffc7884358
Lucie <lucie.konecna@ucdconnect.ie>
2020-01-01 14:15:09 +0000

    paint face exercise

--- BAB/HappyFace/src/main/Game.java
+++ BAB/HappyFace/src/main/Game.java
@@ -1,4 +1,15 @@
 package main;
 
+import javax.swing.*;
+
 public class Game {
+    public static void main(String[] args) {
+        JFrame window = new JFrame("Happy Face Program");
+        window.setContentPane(new GamePanel());
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setLocationRelativeTo(null);
+        window.pack();
+        window.setVisible(true);
+    }
 }
--- BAB/HappyFace/src/main/GamePanel.java
+++ BAB/HappyFace/src/main/GamePanel.java
@@ -9,10 +9,48 @@ public class GamePanel extends JPanel {
     public static final int HEIGHT = 480;
 
     public GamePanel() {
-        setPreferredSize(new Dimensions());
+        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setFocusable(true);
+        requestFocus();
     }
 
-    private void paintComponent(Graphics g) {
-
+    protected void paintComponent(Graphics g) {
+        //TODO: paint face
+        super.paintComponent(g);
+        //bg
+        g.setColor(Color.magenta);
+        g.fillRect(0,0, WIDTH, HEIGHT);
+        //face
+        int faceRectPointX = 0;
+        int faceRectPointY = 0;
+        int faceRectWidth = WIDTH;
+        int faceRectHeight = HEIGHT;
+        g.setColor(new Color(150,150,150));
+        g.fillOval(faceRectPointX,faceRectPointY, faceRectWidth, faceRectHeight);
+        g.setColor(Color.black);
+        g.drawOval(faceRectPointX, faceRectPointY, faceRectWidth, faceRectHeight);
+        //eyes
+        int eye1CenterX = (faceRectWidth - faceRectPointX) / 3;
+        int eyeCenterY = (faceRectHeight - faceRectPointY) / 3;
+        int eye2CenterX = 2 * ( (faceRectWidth - faceRectPointX) / 3 );
+        int eyeRadius = 50;
+        int eye1RectPointX = eye1CenterX - eyeRadius;
+        int eye1RectPointY = eyeCenterY - eyeRadius;
+        int eye2RectPointX = eye2CenterX - eyeRadius;
+        int eye2RectPointY = eyeCenterY - eyeRadius;
+        g.setColor(new Color( 50, 150, 50));
+        g.fillOval(eye1RectPointX, eye1RectPointY, eyeRadius, eyeRadius);
+        g.fillOval(eye2RectPointX, eye2RectPointY, eyeRadius, eyeRadius);
+        // mouth arc
+        int mouthCenterX = faceRectWidth / 2;
+        int mouthCenterY = 2 * (faceRectHeight / 3);
+        int mouthWidth = eyeRadius * 3;
+        int mouthHeight = eyeRadius * 2;
+        int mouthRectPointX = mouthCenterX - (mouthWidth / 2);
+        int mouthRectPointY = mouthCenterY - (mouthHeight / 2);
+        int mouthFromAngle = 210;
+        int mouthToAngle = 120;
+        g.setColor(Color.black);
+        g.drawArc(mouthRectPointX, mouthRectPointY, mouthWidth, mouthHeight, mouthFromAngle, mouthToAngle);
     }
 }

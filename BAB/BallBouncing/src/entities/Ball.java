0d2d99f0020b32de2e7688a52a84e3b93c2a698e
Lucie <lucie.konecna@ucdconnect.ie>
2020-01-02 13:13:40 +0000

    updated Vector2D and Ball classes

---
+++ BAB/BallBouncing/src/entities/Ball.java
@@ -0,0 +1,13 @@
+package entities;
+
+import math.Vector2D;
+
+import java.awt.*;
+
+public class Ball {
+    private Point point;
+    private int radius;
+    private Vector2D displacement;
+
+    public Ball() {}
+}

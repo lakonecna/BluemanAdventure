b9b23a589ea9f318bae77955a03a197527eaea6b
Lucie <lucie.konecna@ucdconnect.ie>
2020-01-04 22:07:59 +0000

    path issue fixed temporarily by duplicating GifDecoder class

--- BAB/AnimatedGIF/src/entities/AnimatedGIF.java
+++ BAB/AnimatedGIF/src/entities/AnimatedGIF.java
@@ -1,4 +1,17 @@
 package entities;
 
+import imageprocessing.GifDecoder;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+
 public class AnimatedGIF {
+    private Point location;
+    private BufferedImage[] frames;
+    private int frameCount = -1;
+    private int currentFrame = -1;
+    private boolean x = false; //TODO
+    private GifDecoder decoder;
+
+    public AnimatedGIF
 }
